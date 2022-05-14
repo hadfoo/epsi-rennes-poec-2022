44aa04684e2dc00b8b6fad5d862bd0c0ec54063b
Bob-117 <bricaud.olivier.dev@gmail.com>
2022-05-14 22:08:23 +0200

    add GuitarShopHome.vue

---
+++ src/guitar-shop-vue/src/components/GuitarShopHome.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Home</h1>
+  <h3> {{ title }} </h3>
+
+</template>
+
+<script>
+export default {
+  name: 'GuitarShopHome',
+  data () {
+    return {
+      title: '',
+    }
+  },
+  mounted() {
+    fetch("/GuitarShop/public")
+      .then((response) => response.text())
+      .then((data) => {
+        this.title = data;
+      })
+
+  }
+}
+</script>
