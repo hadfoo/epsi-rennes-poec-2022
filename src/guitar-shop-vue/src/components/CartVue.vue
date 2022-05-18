b43f995df106d314253a5dbf70d79206b85718ce
Bob-117 <bricaud.olivier.dev@gmail.com>
2022-05-18 02:49:41 +0200

    Cart and Product Vue

---
+++ src/guitar-shop-vue/src/components/CartVue.vue
@@ -0,0 +1,26 @@
+<template >
+
+  <div id="cart" style="position: fixed; right: 10%">
+    <h1>cart</h1>
+
+
+  </div>
+
+</template>
+
+
+<script>
+
+export default {
+  name: 'CartVue',
+  components: {
+
+  },
+  data() {
+    return {
+      purchased_products: '',
+      //current_login: current_login
+    }
+  },
+}
+</script>
