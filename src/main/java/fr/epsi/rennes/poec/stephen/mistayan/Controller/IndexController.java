310c01e9165a8dd79729dcc4bf05dbe0760c96d2
Mistayan <Helixmastaz@gmail.com>
2022-05-25 01:24:52 +0200

    dernier en date: userController ajout get /user/orders

--- src/main/java/fr/epsi/rennes/poec/stephen/mistayan/Controller/IndexController.java
+++ src/main/java/fr/epsi/rennes/poec/stephen/mistayan/Controller/IndexController.java
@@ -5,6 +5,8 @@ import fr.epsi.rennes.poec.stephen.mistayan.domain.Pizza;
 import fr.epsi.rennes.poec.stephen.mistayan.domain.Response;
 import fr.epsi.rennes.poec.stephen.mistayan.service.PanierService;
 import fr.epsi.rennes.poec.stephen.mistayan.service.PizzaService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.eclipse.jdt.internal.compiler.codegen.ConstantPool.GetClass;
 
 /**
  * Author: Stephen Mistayan
@@ -22,18 +26,11 @@ import java.util.List;
 
 @RestController
 public class IndexController {
+    private static final Logger logger = LogManager.getLogger(GetClass);
     @Autowired
     private PizzaService pizzaService;
     @Autowired
     private PanierService panierService;
-
-//    @GetMapping("/user/pizza")
-//    public Response<List<Pizza>> getAllPizzas() {
-//        List<Pizza> pizzas = pizzaService.getAllPizzas();
-//        Response<List<Pizza>> response = new Response<>();
-//        response.setData(pizzas);
-//        return response;
-//    }
 
     @GetMapping("/public/pizza")
     public Response<List<Pizza>> getAllPizzasPublic() {
@@ -45,24 +42,25 @@ public class IndexController {
 
     @PostMapping("/public/panier/pizza")
     public Response<Integer> actionPizza(
-            @RequestParam int pizza_id,
-            @RequestParam int panier_id,
+            @RequestParam int pizzaId,
+            @RequestParam int panierId,
             @RequestParam int action) {
         Response<Integer> response = new Response<>();
         if (action == 1) {
+            logger.info("##User Action :: /public/panier/pizza/? " + pizzaId + " & " + panierId + " & " + action);
             Pizza pizza = new Pizza();
-            pizza.setId(pizza_id);
-            panier_id = panierService.addPizza(pizza, panier_id);
-        } else { //pizza_id + 1 car index commence à 0 en json
-            panier_id = panierService.remPizza(pizza_id, panier_id);
+            pizza.setId(pizzaId);
+            panierId = panierService.addPizza(pizza, panierId);
+        } else {
+            panierId = panierService.remPizza(pizzaId, panierId);
         }
-        response.setData(panier_id);
+        response.setData(panierId);
         return response;
     }
 
     @GetMapping("/public/panier")
-    public Response<Panier> getPanier(@RequestParam int panier_id) {
-        Panier panier = panierService.getPanierById(panier_id);
+    public Response<Panier> getPanier(@RequestParam int panierId) {
+        Panier panier = panierService.getPanierById(panierId);
 
         Response<Panier> response = new Response<>();
         response.setData(panier);
--- src/main/java/fr/epsi/rennes/poec/stephen/mistayan/Controller/UserController.java
+++ src/main/java/fr/epsi/rennes/poec/stephen/mistayan/Controller/UserController.java
@@ -1,21 +1,23 @@
 package fr.epsi.rennes.poec.stephen.mistayan.Controller;
 
 
+import fr.epsi.rennes.poec.stephen.mistayan.domain.Commande;
 import fr.epsi.rennes.poec.stephen.mistayan.domain.Response;
 import fr.epsi.rennes.poec.stephen.mistayan.domain.User;
 import fr.epsi.rennes.poec.stephen.mistayan.exception.TechnicalException;
 import fr.epsi.rennes.poec.stephen.mistayan.service.UserService;
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -32,30 +34,46 @@ public class UserController {
             user.setPassword(password);
             userService.addUser(user);
         } catch (TechnicalException e) {
-            throw new TechnicalException(new SQLException("public/register route user.service.addUser failed"));
+            throw new RuntimeException("public/register route user.service.addUser failed");
         }
 
     }
 
     @PostMapping("/user/order")
-    public Response<Long> order(@RequestParam int panier_id) {
+    public Response<Long> order(@RequestParam int panierId) {
 //                                @AuthenticationPrincipal User user) {
         Response<Long> response = new Response<>();
         try {
-            logger.info("########### Action : ordering");
+            logger.info("########### Action : ordering cart " + panierId);
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-            long order_id = userService.userOrder(auth.getName(), panier_id);
-            response.setData(order_id);
+            userService.userOrder(auth.getName(), panierId);
             response.setSuccess(true);
             return response;
         } catch (TechnicalException e) {
             response.setSuccess(false);
-            logger.log(Level.FATAL, "user/order route userService.userOrder failed :::> " + e);
-            return (response);
-//            throw new TechnicalException(new SQLException(""));
+            logger.fatal("user/order route userService.userOrder failed :::> " + e);
+            throw new RuntimeException("order failed. See logs for more details");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new TechnicalException(e);
         }
+    }
 
+    @GetMapping("/user/orders")
+    public Response<List<Commande>> orders() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        // on récupère le nom de l'utilisateur via l'application, et son token JSession
+        Response<List<Commande>> response = new Response<>();
+        try {
+            int userId = userService.getUserIdFromName(auth.getName());
+            logger.info("########### Action : fetching orders from userId : " + userId);
+            List<Commande> commandes = userService.getUserIdOrders(userId);
+            logger.info("########### Fetched Commande :  " + commandes);
+            response.setData(commandes);
+            response.setSuccess(true);
+            return response;
+        } catch (SQLException e) {
+            response.setSuccess(false);
+            throw new RuntimeException("Could not load user's orders");
+        }
     }
 }
